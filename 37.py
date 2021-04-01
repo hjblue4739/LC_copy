5525b42d1dfe9579d789c2a3011bd7335ede7b32
hjblue4739 <hjblue4739@hotmail.com>
2021-04-01 08:42:31 -0400

    Update 37.py

--- 37.py
+++ 37.py
@@ -1,5 +1,41 @@
 class Solution:
     def solveSudoku(self, board):
+        from collections import defaultdict as d
+        rows, cols, triples, tofill = d(set), d(set), d(set), []
+        for r in range(9):
+            for c in range(9):
+                x, t = board[r][c], (r // 3, c // 3)
+                if x != ".":
+                    rows[r].add(x)
+                    cols[c].add(x)
+                    triples[(r // 3, c // 3)].add(x)
+                else:
+                    tofill.append((r, c, t))
+        def dfs():
+            #print(board[-1])
+            if not tofill:
+                return True
+            r, c, t = tofill[-1]
+            for dig in "123456789": 
+                if dig not in rows[r] | cols[c] | triples[t]:
+                    board[r][c] = dig
+                    rows[r].add(dig)
+                    cols[c].add(dig)
+                    triples[t].add(dig)
+                    tofill.pop()
+                    if dfs(): return True
+                    board[r][c] = "."
+                    rows[r].discard(dig)
+                    cols[c].discard(dig)
+                    triples[t].discard(dig)
+                    tofill.append((r,c,t))
+            return False
+        dfs()
+        
+
+###original
+class Solution:
+    def solveSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
