3f880e8f32e8ec362590c68d2134838dea83d76d
hjblue4739 <hjblue4739@hotmail.com>
2021-11-05 16:00:47 -0400

    Update 314.py

--- 314.py
+++ 314.py
@@ -1,3 +1,29 @@
+#Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+
+#If two nodes are in the same row and column, the order should be from left to right.
+'''
+Input: 
+[3,9,20,null,null,15,7]
+
+   3
+  /\
+ /  \
+ 9  20
+    /\
+   /  \
+  15   7 
+
+Output:
+
+[
+  [9],
+  [3,15],
+  [20],
+  [7]
+]
+'''
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
