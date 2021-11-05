4177ce11d04405fb44cbfd35de74c2f1abb48bc8
hjblue4739 <hjblue4739@hotmail.com>
2021-11-05 00:47:33 -0400

    Update 536.py

--- 536.py
+++ 536.py
@@ -1,3 +1,21 @@
+'''
+https://www.cnblogs.com/grandyang/p/6793904.html
+You need to construct a binary tree from a string consisting of parenthesis and integers.
+
+The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
+
+You always start to construct the left child node of the parent first if it exists.
+
+Example:
+
+Input: "4(2(3)(1))(6(5))"
+Output: return the tree root node representing the following tree:
+
+       4
+     /   \
+    2     6
+   / \   / 
+'''
 class treeNode: 
     def __init__(self, x = 0, left = None , right = None): 
          self.val = x 
