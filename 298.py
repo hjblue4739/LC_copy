459e973a4b878d90d04116e2513f25ca5d595ae6
hjblue4739 <hjblue4739@hotmail.com>
2021-11-07 01:02:53 -0400

    Update 298.py

--- 298.py
+++ 298.py
@@ -5,21 +5,33 @@
 #         self.left = None
 #         self.right = None
 
-
+###consecutive, and increasing such as [1,2,3]
 class Solution:
     def longestConsecutive(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
         if not root: return 0
         result = 0
         stack = [(root, 1)]
         while stack:
             node, curr = stack.pop()
-            if node.left:
-                stack.append((node.left, curr+1 if node.left.val == node.val+1 else 1))
-            if node.right:
-                stack.append((node.right, curr+1 if node.right.val == node.val+1 else 1))
             result = max(result, curr)
+            l, r, v = node.left, node.right, node.val
+            for x in [l,r]: 
+                if x:
+                    stack.append((x, curr+1 if x.val == v+1 else 1))
         return result
+    
+### increasing only [1,5,7]
+class Solution:
+    def longestConsecutive(self, root):
+        if not root: return 0
+        result = 0
+        stack = [(root, 1)]
+        while stack:
+            node, curr = stack.pop()
+            result = max(result, curr)
+            l, r, v = node.left, node.right, node.val
+            for x in [l,r]: 
+                if x:
+                    stack.append((x, curr+1 if x.val > v else 1))
+        return result
+    
