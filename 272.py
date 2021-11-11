934a9124226698873a8782c1ee20c8bf16649714
hjblue4739 <hjblue4739@hotmail.com>
2021-11-11 10:42:00 -0500

    Update 272.py

--- 272.py
+++ 272.py
@@ -4,34 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def closestKValues(self, root: TreeNode, target: float, k: int) -> List[int]:
-        predecessor, successor = [], []
-        self.getPredecessor(root, target, predecessor)
-        self.getSuccessor(root, target, successor)
-        result = []
-        while k > 0:
-            if not predecessor:
-                result.append(successor.pop())
-            elif not successor:
-                result.append(predecessor.pop())
-            elif abs(predecessor[-1] - target) < abs(successor[-1] - target):
-                result.append(predecessor.pop())
-            else:
-                result.append(successor.pop())
-            k -= 1
-        return result
 
-    def getPredecessor(self, node, target, stack):
-        if not node: return
-        self.getPredecessor(node.left, target, stack)
-        if node.val >= target: return
-        stack.append(node.val)
-        self.getPredecessor(node.right, target, stack)
+###similar to 270 and 685 
 
-    def getSuccessor(self, node, target, stack):
-        if not node: return
-        self.getSuccessor(node.right, target, stack)
-        if node.val < target: return
-        stack.append(node.val)
-        self.getSuccessor(node.left, target, stack)
+class tree: 
+    def findKClosest(self, root, target, k): 
+        def inorder(x): 
+            return inorder(x.left) + [x.val] + inorder(x.right) if x else [] 
+        
+        seq = inorder(root)
+        left, right = 0, len(seq) - 1 
+        while left + k - 1 < right:  ### note: k - 1 rather k 
+            if abs(target - seq[left]) <= abs(target - seq[right]): 
+                right -= 1 
+            else: 
+                left += 1 
+        return seq[left:right + 1] ### note: right + 1
